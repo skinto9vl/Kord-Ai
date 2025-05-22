2fd1abd9d764a2f7a1843ff5902303acf6632892
skinto9vl <itzdizzykane@gmail.com>
2025-05-22 08:02:54 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,17 +2,17 @@
 
 global.settings = {
   OWNER_NUMBERS: "2347048825904", // your number with country code
-  OWNER_NAME: 'Skinto', // your owner name 
+  OWNER_NAME: 'ȘκίɴτΘ', // your owner name 
   BOT_NAME: "KORD", // your preferred bot name
   SESSION_ID: 'kord_ai-mMTc36tdjJJb', // replace your_sessionid with yours and make sure its inside rhe quotes
   INVINCIBLE_MODE: false, // toogle invincible mode 
-  WORK_MODE: "Public",
+  WORK_MODE: "Private",
   TIME_ZONE: "Africa/Lagos",
   ANTI_VIEWONCE: true,
   READ_ALL_MESSAGES: false,
   AUTO_READ_STATUS: true,
   PREFIX: [".", "/", "!"],
-  GROUP_EVENTS: true,
+  GROUP_EVENTS: false,
   WELCOME_MESSAGE: "Hello! I'm KORD-AI, your friendly WhatsApp bot.",
   DEFAULT_TRANSLATION_LANG: "en",
   COMMAND_COOLDOWN_TIME_IN_MS: 0.01,
